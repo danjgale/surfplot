2eb4eabbe1204ac55a8ec62c7a507dfc91f16113
Dan Gale <danjgale@gmail.com>
2021-06-30 16:30:37 -0600

    added necessary keys for pypi

--- setup.py
+++ setup.py
@@ -22,13 +22,13 @@ setup(
     version=__version__,
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*",
                                     "tests"]),
-    license='MIT',
+    license='BSD-3',
     author='Dan Gale',
     maintainer_email="d.gale@queensu.ca",
     description=("A high-level interface for Brainspace's surface plotting "
                  "capabilities"),
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type='text/x-rst',
     url='https://github.com/danjgale/brainplot',
     python_requires='>=3.7.0',
     install_requires=[
@@ -38,10 +38,15 @@ setup(
         'numpy>=1.16.5',
         'vtk'
     ],
+    package_data={
+        'brainplot': ['data/*']
+    },
+    include_package_data=True,
     tests_require=test_deps,
     extras_require=extras,
     setup_requires=['pytest-runner'],
     classifiers=[
+        'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
         'Intended Audience :: Science/Research',
