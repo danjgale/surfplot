5802300a360f29ba28ab5a6a30e2c17b73d45fca
Dan Gale <danjgale@gmail.com>
2021-07-03 09:41:36 -0600

    cache examples

---
+++ docs/auto_examples/plot_tutorial_02.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+.. _tutorial02_ref:
+
+Tutorial 02: Types of Surfaces
+==============================
+
+This tutorial covers what types of surfaces that can be plotted with 
+``brainplot``. 
+
+Types of surfaces
+-----------------
+
+Put briefly, ``brainplot`` can take file paths to any valid surface file(s) 
+with geometry data. Under the hood, :class:`brainplot.plotting.Plot` runs 
+:func:`brainspace.mesh.mesh_io.read_surface` to load files. Typically, these
+will be Freesurfer or GIFTI files. 
+
+:class:`Plot` can also read instances of 
+:class:`brainspace.vtk_interface.wrappers.data_object.BSPolyData`, which are 
+returned by :func:`read_surface`. So, pre-loaded surfaces with 
+:func:`read_surface` can be plotted as well.
+
+Beyond that, ``brainplot`` is invariant to the actual brain surfaces you wish
+to use. Common human surfaces include `fsaverage` surfaces packaged with 
+Freesurfer, and Human Connectome Project `fsLR` surfaces (`downloadable here 
+<https://balsa.wustl.edu/reference/show/pkXDZ>`_). Several different 
+human surfaces can also all be found on OSF `here <https://osf.io/4mw3a/>`_. 
+Non-human surfaces can of course also be plotted, such as the `NMTv2 Macaque 
+surfaces <https://afni.nimh.nih.gov/pub/dist/doc/htmldoc/nonhuman/macaque_tempatl/template_nmtv2.html>`_.
+
+Note that throughout these tutorials, surfaces are automatically fetched using
+``brainnotation`` or  ``Brainspace`` to avoid having to specify local files. It
+is also possible to `fetch fsaverage surfaces <https://nilearn.github.io/modules/generated/nilearn.datasets.fetch_surf_fsaverage.html#nilearn.datasets.fetch_surf_fsaverage>`_
+using ``nilearn``. These are all great options to automically get surfaces in
+your workflow, and make reproducibility and portability of your code a bit 
+more feasible.
+
+.. note::
+    Make sure to *always* use the correct surface for your data. Double check 
+    the number of vertices in both your data and the surfaces you are using for
+    plotting.
+
+In :ref:`sphx_glr_auto_examples_plot_tutorial_01.py` we plotted `fsLR` 
+surfaces. For the sake of demonstration, let's plot Freesurfer's `fsaverage` 
+here, again using ``brainnotation`` fetching functions.
+"""
+from brainnotation.datasets import fetch_fsaverage
+from brainplot import Plot
+
+surfaces = fetch_fsaverage(density='164k')
+lh, rh = surfaces['inflated']
+
+# make figure
+p = Plot(lh, rh)
+fig = p.build()
+fig.show()
+###############################################################################
+# Brightness
+# ----------
+#
+# By default, :class:`Plot` will plot a medium-gray surface, typical of most
+# surface plotting packages like Connectome Workbench. The brightness of the
+# blank surface can be adjusted using the `brightness` parameter, if desired.
+# Values range from 0 (black) to 1 (white). For example:  
+p = Plot(lh, rh, brightness=.8)
+fig = p.build()
+fig.show()
---
+++ docs/auto_examples/plot_tutorial_04.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+.. _tutorial04_ref:
+
+Tutorial 04: Layouts and Views
+==============================
+
+This tutorial covers the layout and view options provided by ``brainplot``.
+
+For variety, let's import the left and right Conte69 midthickness surface
+directly using :func:`brainspace.datasets.load_conte69`. Then, we'll make a 
+blank surface plot using the default layout and view, which is a 2x2 'grid' of 
+lateral and medial views that is identical to the default setup in Connectome 
+Workbench.
+"""
+from brainspace.datasets import load_conte69
+from brainplot import Plot
+
+lh, rh = load_conte69()
+p = Plot(lh, rh)
+fig = p.build()
+fig.show()
+###############################################################################
+# Layout
+# ------
+#
+# The layout can be adjusted with the `layout` parameter, and has 3 options: 
+# 'grid' (default, shown above), 'row', or 'column'. The `size` and `zoom` 
+# parameters will have to be adjusted based on the layout and number of views. 
+#
+# Above we see that 'grid' gives us a views-by-hemisphere grid, where the left
+# hemisphere is the left column and the right hemisphere is the right column. 
+# Meanwhile, the 'row' layout gives a single horizontal row of brains: 
+p = Plot(lh, rh, size=(800, 200), zoom=1.2, layout='row')
+fig = p.build()
+fig.show()
+###############################################################################
+# The 'column' layout gives a single vertical column of brains.
+p = Plot(lh, rh, size=(200, 600), zoom=1.6, layout='column')
+fig = p.build()
+fig.show()
+###############################################################################
+# As well, it's also possible to plot just one hemisphere. If the layout is 
+# set as default ('grid'), then a single hemisphere is plotted as row:
+p = Plot(lh, size=(400, 200), zoom=1.2)
+fig = p.build()
+fig.show()
+###############################################################################
+# Views
+# -----
+#
+# ``brainplot`` makes it easy to configure the view(s) you wish to use. One or
+# more views can be specified through the `views` parameter. As we've seen 
+# before, the default is to include lateral and medial views. It is also 
+# possible to show just one view:
+p = Plot(lh, rh, size=(400, 200), zoom=1.2, views='lateral')
+fig = p.build()
+fig.show()
+###############################################################################
+# It is also possible to show more than just lateral and medial views, such
+# as 'posterior'. Note that views are plotted in order in which they appear
+# in the list:
+p = Plot(lh, rh, size=(500, 400), zoom=1.4,  views=['lateral', 'posterior'])
+fig = p.build()
+fig.show()
+###############################################################################
+# All possible views are shown here (the right hemisphere for brevity):
+all_views = ['lateral', 'medial', 'dorsal', 'ventral', 'anterior', 'posterior']
+p = Plot(surf_rh=rh, size=(900, 200), zoom=.8, layout='row', views=all_views)
+fig = p.build()
+fig.show()
+###############################################################################
+# Finally, it is possible to flip the left and right hemisphere. This is 
+# useful when plotting just the 'anterior' or 'ventral' for both hemispheres.
+# For example: 
+p = Plot(lh, rh, size=(200, 200), zoom=3, views='anterior', flip=True)
+fig = p.build()
+fig.show()
